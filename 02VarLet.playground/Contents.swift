1ec13fb513609119cec8b2c7f9520a16d08204dc
Uriel Olascoaga <uriel1396@gmail.com>
2023-03-14 22:13:25 -0600

    Added variables and constants declaration

---
+++ 02VarLet.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+let maxLoginAttempts = 3
+var currentLoginAttempt = 0
+var x = 0.0,
+    y = 0.0,
+    z = 0.0
+var welcomeMessage: String
+var red, green, blue: Double
+
+welcomeMessage = "Que Pachuca por Toluca"
