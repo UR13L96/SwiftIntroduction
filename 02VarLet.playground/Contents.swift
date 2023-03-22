5be20efd5a2c9a886b977b2e616f960630e9efb1
Uriel Olascoaga <uriel1396@gmail.com>
2023-03-21 23:44:40 -0600

    Added code comments

--- 02VarLet.playground/Contents.swift
+++ 02VarLet.playground/Contents.swift
@@ -11,3 +11,12 @@ var red, green, blue: Double
 welcomeMessage = "Que Pachuca por Toluca"
 
 print("The current attempt is: \(currentLoginAttempt) of an available total of: \(maxLoginAttempts)")
+
+// This is a comment
+
+/*
+ This is a comment
+ Takes up multiple lines
+ Here I can write a lot of information
+*/
+
