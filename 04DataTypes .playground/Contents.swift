14961b6aedd2ec20780ab0faeb5d114ae7093f96
Uriel Olascoaga <uriel1396@gmail.com>
2023-04-17 22:51:44 -0600

    Added new playground for datatypes different to numbers like bool

---
+++ 04DataTypes .playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let orangesAreOrange: Bool = true
+let foodIsDelicious: Bool = false
+
+var isAged: Bool
+isAged = true
+
+if isAged {
+    print("You can join to the party")
+} else {
+    print("You can't join to the party")
+}
+
+var age = 31
+if age >= 18 {
+    print("You can join to the party")
+}
