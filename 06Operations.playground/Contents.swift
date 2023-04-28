2f364a75fc74cb5d936a4d9743d8d754d88234c8
Uriel Olascoaga <uriel1396@gmail.com>
2023-04-27 22:33:41 -0600

    Added assignment and arithmetic operators example

---
+++ 06Operations.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let b = 10
+var a = 5
+a = b
+
+let (x, y) = (1, 2)
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+9%4
+-9%4
+
+let five = 5
+let minusFive = -five
+let plusFive = -minusFive
+
+var number = 5
+number += 3
+number -= 2
+number *= 4
+number /= 8
