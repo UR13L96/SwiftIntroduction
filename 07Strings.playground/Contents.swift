9d7f2db2af9facbd536434c2263b2baeeba75271
Uriel Olascoaga <uriel1396@gmail.com>
2023-05-09 22:28:52 -0600

    Added String playground with multiple String examples

---
+++ 07Strings.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+let string = "This a string"
+
+let multilineString = """
+Hi, I'm Uriel\
+ We are studying Swift's course
+Greetings
+"""
+
+print(multilineString)
+
+let wiseWords = "\"Life is like riding a bicycle. To keep your balance, you must keep moving.\" - Albert Einstein"
+let dolarSign = "\u{24}"
+let blackHeart = "\u{2665}"
+let heart = "\u{1F496}"
