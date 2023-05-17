2cd39e61ce5a4ad3416819add3c0d42b31c6a029
Uriel Olascoaga <uriel1396@gmail.com>
2023-05-16 22:39:28 -0600

    Added string concatenation example and value type for Strings

--- 07Strings.playground/Contents.swift
+++ 07Strings.playground/Contents.swift
@@ -14,3 +14,26 @@ let wiseWords = "\"Life is like riding a bicycle. To keep your balance, you must
 let dolarSign = "\u{24}"
 let blackHeart = "\u{2665}"
 let heart = "\u{1F496}"
+
+var emptyString = ""
+var anotherEmptyString = String()
+
+if emptyString.isEmpty {
+    print("Empty string")
+} else {
+    print("String has value")
+}
+
+var someString = "A horse"
+someString += " and carriage"
+someString += " and soldier"
+
+var a = "A"
+var b = "B"
+print("a: \(a), b: \(b)")
+b = a
+print("a: \(a), b: \(b)")
+b = "C"
+print("a: \(a), b: \(b)")
+a = "D"
+print("a: \(a), b: \(b)")
