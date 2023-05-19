d15b8d8d9b71977482572c4ffe27b34aa957255b
Uriel Olascoaga <uriel1396@gmail.com>
2023-05-18 21:16:17 -0600

    Added sufix and prefix function usage

--- 07Strings.playground/Contents.swift
+++ 07Strings.playground/Contents.swift
@@ -84,3 +84,29 @@ greeting
 let index = greeting.firstIndex(of: ",") ?? greeting.endIndex
 let firstPart = greeting[..<index]
 let newString = String(firstPart)
+
+let newGreeting = "Hi, I'm Uriel Olascoaga"
+newGreeting.hasPrefix("Hi")
+newGreeting.hasSuffix("bye")
+
+let collection = [
+    "Act 1 Scene 1",
+    "Act 1 Scene 2",
+    "Act 1 Scene 3",
+    "Act 1 Scene 4",
+    "Act 1 Scene 5",
+    "Act 2 Scene 1",
+    "Act 2 Scene 2",
+    "Act 2 Scene 3",
+    "Act 3 Scene 1",
+    "Act 3 Scene 2"
+]
+
+var act1SceneCount = 0
+
+for scene in collection {
+    if scene.hasPrefix("Act 1") {
+        act1SceneCount += 1
+    }
+}
+print("The number of scenes in Act 1 is: \(act1SceneCount)")
