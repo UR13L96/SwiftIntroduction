29287b08500d3a1352fd5201b5b06dce922234c5
Uriel Olascoaga <uriel1396@gmail.com>
2023-06-26 22:09:11 -0600

    Added control flow playground with for in loop examples

---
+++ 09ControlFlow.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+let names = ["Uriel", "Nadia", "Kurt"]
+
+for name in names {
+    print("Hello \(name)!")
+}
+
+let numberOfLegs = [
+    "spider": 8,
+    "ant": 6,
+    "dog": 4
+]
+
+for (animal, legs) in numberOfLegs {
+    print("\(animal): \(legs) legs")
+}
+
+for i in 1...5 {
+    print("\(i) x 3 = \(i * 3)")
+}
+
+let base = 2
+let power = 10
+var answer = 1
+
+for _ in 1...power {
+    answer *= base
+    print(answer)
+}
+
+var hour = 9
+let minutes = 60
+let minuteInterval = 5
+for minute in stride(from: 0, to: minutes, by: minuteInterval) {
+    print("\(hour):\(minute)")
+}
