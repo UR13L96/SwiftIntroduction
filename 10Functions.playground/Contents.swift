f5fefa324b03b30007656a7998762711588bf797
Uriel Olascoaga <uriel1396@gmail.com>
2024-02-13 16:42:26 -0600

    Added function declaration

---
+++ 10Functions.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+func greeting(person: String) -> String {
+    return "Hello, \(person)!"
+}
+
+print(greeting(person: "Ian"))
+print(greeting(person: "Guillermo"))
+print(greeting(person: "David"))
